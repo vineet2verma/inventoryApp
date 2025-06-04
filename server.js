5c50d4cabb189706b15cb611caac564d93e6aecc
vineet2verma <vineet2verma@gmail.com>
2025-06-04 23:31:07 +0530

    socket configure testing on vercel

---
+++ server.js
@@ -0,0 +1,36 @@
+import { createServer } from "node:http";
+import next from "next";
+import { Server } from "socket.io";
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer);
+
+  io.on("connection", (socket) => {
+    socket.broadcast.emit("message", "world");
+
+    socket.on("update-morbi", (value) => {
+      console.log("value => ", value);
+      socket.broadcast.emit("fetch-morbi-data", "true");
+    });
+
+    console.log("connted socket id => ", socket.id);
+  });
+
+  httpServer
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
